d8d68820cc81fc96e69fe85317df67d1b49d9360
Nandhakumar K <116391685+Tamilupdates@users.noreply.github.com>
2023-05-21 20:06:37 +0530

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/codewithweeb/mirror-with-weeb"
+    UPSTREAM_REPO = "https://github.com/Tamilupdates/ML"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
